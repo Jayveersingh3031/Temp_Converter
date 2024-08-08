670af651bc53dd8e2555039c82670ef41d75b540
jayveersingh3031 <jayveersinghpawar03@gmail.com>
2024-08-08 11:08:31 +0530

    temp_converter

---
+++ script.js
@@ -0,0 +1,62 @@
+const temperature = document.querySelector(".temperature input");
+const result = document.querySelector(".result input");
+const inputTemperatureUnit = document.querySelector(".temperature select");
+const resultTemperatureUnit = document.querySelector(".result select");
+let btn = document.getElementById("convert_btn");
+let inp1 = document.getElementById("input1");
+let inp2 = document.getElementById("input2");
+temperature.oninput = function () {
+  let inputTemperatureUnitValue = inputTemperatureUnit.value;
+  let resultTemperatureUnitValue = resultTemperatureUnit.value;
+
+  // From Celcius To Other Units
+  let CelciusToKelvin = inputTemperatureUnitValue === "Celcius" && resultTemperatureUnitValue === "Kelvin";
+  let CelciusToFahrenheit = inputTemperatureUnitValue === "Celcius" && resultTemperatureUnitValue === "Fahrenheit";
+
+  // From Kelvin to Other Units
+  let KelvinToCelcius = inputTemperatureUnitValue === "Kelvin" && resultTemperatureUnitValue === "Celcius";
+  let KelvinToFahrenheit = inputTemperatureUnitValue === "Kelvin" && resultTemperatureUnitValue === "Fahrenheit";
+
+  // From Fahrenheits to Other Units
+  let FahrenheitToCelcius = inputTemperatureUnitValue === "Fahrenheit" && resultTemperatureUnitValue === "Celcius";
+  let FahrenheitToKelvin = inputTemperatureUnitValue === "Fahrenheit" && resultTemperatureUnitValue === "Kelvin";
+
+  // From the Same Unit to Same unit
+  let sameUnits = inputTemperatureUnitValue === resultTemperatureUnitValue;
+
+  // Calculations...
+  let inputTemperatureValue = temperature.value;
+  let convertTemperature = 0;
+
+  if(CelciusToKelvin) {
+    convertTemperature = (inputTemperatureValue * 1) + 273.15;
+    result.value = convertTemperature.toFixed(2);
+  }
+  else if (CelciusToFahrenheit) {
+    convertTemperature = (9/5 * inputTemperatureValue) + 32;
+    result.value = convertTemperature.toFixed(2);
+  }
+  else if (KelvinToCelcius) {
+    convertTemperature = (inputTemperatureValue - 273.15);
+    result.value = convertTemperature.toFixed(2);
+  }
+  else if (KelvinToFahrenheit) {
+    convertTemperature = 9/5 * (inputTemperatureValue - 273.15) + 32;
+    result.value = convertTemperature.toFixed(2);
+  }
+  else if (FahrenheitToCelcius) {
+    convertTemperature = 5/9 * (inputTemperatureValue - 32);
+    result.value = convertTemperature.toFixed(2);
+  }
+  else if (FahrenheitToKelvin) {
+    convertTemperature = 5/9 * (inputTemperatureValue - 32) + 273.15;
+    result.value = convertTemperature.toFixed(2);
+  }
+  else if (sameUnits) {
+    result.value = inputTemperatureValue;
+  }
+}
+btn.addEventListener("click",()=>{
+     inp1.value = "";
+     inp2.value = "";
+})
